d7955c173a6b092bca3f568eafb93a76be085f54
Matt Swain <m.swain@me.com>
2013-04-19 16:21:26 +0100

    Reformat code to be PEP 8 compliant

--- pubchempy.py
+++ pubchempy.py
@@ -18,7 +18,6 @@ __email__ = 'm.swain@me.com'
 __version__ = '1.0'
 __license__ = 'MIT'
 
-
 API_BASE = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug'
 
 
@@ -36,18 +35,18 @@ def request(identifier, namespace='cid', domain='compound', operation=None, outp
         identifier = ','.join(str(x) for x in identifier)
 
     # Filter None values from kwargs
-    kwargs = dict((k,v) for k,v in kwargs.iteritems() if v is not None)
+    kwargs = dict((k, v) for k, v in kwargs.iteritems() if v is not None)
 
     # Build API URL
     urlid, postdata = None, None
     if namespace in ['listkey', 'formula'] or (searchtype and namespace == 'cid') or domain == 'sources':
-        urlid = urllib2.quote(identifier.encode('utf8').replace('/','.'))
+        urlid = urllib2.quote(identifier.encode('utf8').replace('/', '.'))
     else:
-        postdata = '%s=%s' % (namespace, urllib2.quote(identifier.encode('utf8').replace('/','.')))
+        postdata = '%s=%s' % (namespace, urllib2.quote(identifier.encode('utf8').replace('/', '.')))
     comps = filter(None, [API_BASE, domain, searchtype, namespace, urlid, operation, output])
     apiurl = '/'.join(comps)
     if kwargs:
-        apiurl+= '?%s' % urllib.urlencode(kwargs)
+        apiurl += '?%s' % urllib.urlencode(kwargs)
 
     # Make request
     try:
@@ -56,6 +55,7 @@ def request(identifier, namespace='cid', domain='compound', operation=None, outp
         return response
     except urllib2.HTTPError as e:
         raise PubChemHTTPError(e)
+
 
 def get(identifier, namespace='cid', domain='compound', operation=None, output='JSON', searchtype=None, **kwargs):
     """ Request wrapper that automatically handles async requests. """
@@ -75,11 +75,13 @@ def get(identifier, namespace='cid', domain='compound', operation=None, output='
         response = request(identifier, namespace, domain, operation, output, searchtype, **kwargs)
     return response
 
+
 def get_compounds(identifier, namespace='cid', searchtype=None, **kwargs):
     """ Retrieve the specified compound records from PubChem. """
     results = json.loads(get(identifier, namespace, searchtype=searchtype, **kwargs))
     compounds = [Compound(r) for r in results['PC_Compounds']]
     return compounds
+
 
 def get_substances(identifier, namespace='sid', **kwargs):
     """ Retrieve the specified substance records from PubChem. """
@@ -87,11 +89,13 @@ def get_substances(identifier, namespace='sid', **kwargs):
     substances = [Substance(r) for r in results['PC_Substances']]
     return substances
 
+
 def get_assays(identifier, namespace='aid', sids=None, **kwargs):
     """ Retrieve the specified assay records from PubChem. """
     results = json.loads(get(identifier, namespace, 'assay', sids, **kwargs))
     assays = [Assay(r) for r in results['PC_AssayContainer']]
     return assays
+
 
 def get_properties(properties, identifier, namespace='cid', searchtype=None, **kwargs):
     if not isinstance(properties, basestring):
@@ -101,10 +105,12 @@ def get_properties(properties, identifier, namespace='cid', searchtype=None, **k
     results = results['PropertyTable']['Properties']
     return results
 
+
 def get_synonyms(identifier, namespace='cid', domain='compound', searchtype=None, **kwargs):
     results = json.loads(get(identifier, namespace, domain, 'synonyms', searchtype=searchtype, **kwargs))
     synonyms = results['InformationList']['Information']
     return synonyms
+
 
 def get_cids(identifier, namespace='name', domain='compound', searchtype=None, **kwargs):
     results = json.loads(get(identifier, namespace, domain, 'cids', searchtype=searchtype, **kwargs))
@@ -114,6 +120,7 @@ def get_cids(identifier, namespace='name', domain='compound', searchtype=None, *
         results = results['InformationList']['Information']
     return results
 
+
 def get_sids(identifier, namespace='cid', domain='compound', searchtype=None, **kwargs):
     results = json.loads(get(identifier, namespace, domain, 'sids', searchtype=searchtype, **kwargs))
     if 'IdentifierList' in results:
@@ -121,6 +128,7 @@ def get_sids(identifier, namespace='cid', domain='compound', searchtype=None, **
     elif 'InformationList' in results:
         results = results['InformationList']['Information']
     return results
+
 
 def get_aids(identifier, namespace='cid', domain='compound', searchtype=None, **kwargs):
     results = json.loads(get(identifier, namespace, domain, 'aids', searchtype=searchtype, **kwargs))
@@ -139,13 +147,16 @@ def get_all_sources(domain='substance'):
     sources = results['InformationList']['SourceName']
     return sources
 
-def download(format, path, identifier, namespace='cid', domain='compound', operation=None, searchtype=None, overwrite=False, **kwargs):
+
+def download(outformat, path, identifier, namespace='cid', domain='compound', operation=None, searchtype=None,
+             overwrite=False, **kwargs):
     """ Format can be  XML, ASNT/B, JSON, SDF, CSV, PNG, TXT.  """
-    response = get(identifier, namespace, domain, operation, format, searchtype, **kwargs)
+    response = get(identifier, namespace, domain, operation, outformat, searchtype, **kwargs)
     if not overwrite and os.path.isfile(path):
         raise IOError("%s already exists. Use 'overwrite=True' to overwrite it." % path)
-    with open(path, 'w') as file:
-        file.write(response)
+    with open(path, 'w') as f:
+        f.write(response)
+
 
 class CacheProperty(object):
     """ Descriptor for caching Molecule properties. """
@@ -159,6 +170,7 @@ class CacheProperty(object):
         if obj is None: return None
         result = obj.__dict__[self.__name__] = self._func(obj)
         return result
+
 
 class Compound(object):
     def __init__(self, record):
@@ -185,9 +197,9 @@ class Compound(object):
     @property
     def atoms(self):
         a = {
-            'x' : self.record['coords'][0]['conformers'][0]['x'],
-            'y' : self.record['coords'][0]['conformers'][0]['y'],
-            'element' : self.record['atoms']['element']
+            'x': self.record['coords'][0]['conformers'][0]['x'],
+            'y': self.record['coords'][0]['conformers'][0]['y'],
+            'element': self.record['atoms']['element']
         }
         if 'z' in self.record['coords'][0]['conformers'][0]:
             a['z'] = self.record['coords'][0]['conformers'][0]['z']
@@ -199,13 +211,13 @@ class Compound(object):
 
     @property
     def bonds(self):
-        bondlist = map(dict, zip(*[[(k, v) for v in value] for k, value in self.record['bonds'].items()]))
+        blist = map(dict, zip(*[[(k, v) for v in value] for k, value in self.record['bonds'].items()]))
         if 'style' in self.record['coords'][0]['conformers'][0]:
             style = self.record['coords'][0]['conformers'][0]['style']
-            for i,annotation in enumerate(style['annotation']):
-                bond = [b for b in bondlist if all(aid in b.values() for aid in [style['aid1'][i], style['aid2'][i]])][0]
+            for i, annotation in enumerate(style['annotation']):
+                bond = [b for b in blist if all(aid in b.values() for aid in [style['aid1'][i], style['aid2'][i]])][0]
                 bond['style'] = annotation
-        return bondlist
+        return blist
 
     @CacheProperty
     def synonyms(self):
@@ -242,68 +254,68 @@ class Compound(object):
 
     @property
     def molecular_formula(self):
-        return parse_prop({'label':'Molecular Formula'}, self.record['props'])
+        return parse_prop({'label': 'Molecular Formula'}, self.record['props'])
 
     @property
     def molecular_weight(self):
-        return parse_prop({'label':'Molecular Weight'}, self.record['props'])
+        return parse_prop({'label': 'Molecular Weight'}, self.record['props'])
 
     @property
     def canonical_smiles(self):
-        return parse_prop({'label':'SMILES','name':'Canonical'}, self.record['props'])
+        return parse_prop({'label': 'SMILES', 'name': 'Canonical'}, self.record['props'])
 
     @property
     def isomeric_smiles(self):
-        return parse_prop({'label':'SMILES','name':'Isomeric'}, self.record['props'])
+        return parse_prop({'label': 'SMILES', 'name': 'Isomeric'}, self.record['props'])
 
     @property
     def inchi(self):
-        return parse_prop({'label':'InChI','name':'Standard'}, self.record['props'])
+        return parse_prop({'label': 'InChI', 'name': 'Standard'}, self.record['props'])
 
     @property
     def inchikey(self):
-        return parse_prop({'label':'InChIKey','name':'Standard'}, self.record['props'])
+        return parse_prop({'label': 'InChIKey', 'name': 'Standard'}, self.record['props'])
 
     @property
     def iupac_name(self):
         # Note: Allowed, CAS-like Style, Preferred, Systematic, Traditional are available in full record
-        return parse_prop({'label':'IUPAC Name','name':'Preferred'}, self.record['props'])
+        return parse_prop({'label': 'IUPAC Name', 'name': 'Preferred'}, self.record['props'])
 
     @property
     def xlogp(self):
-        return parse_prop({'label':'Log P'}, self.record['props'])
+        return parse_prop({'label': 'Log P'}, self.record['props'])
 
     @property
     def exact_mass(self):
-        return parse_prop({'label':'Mass','name':'Exact'}, self.record['props'])
+        return parse_prop({'label': 'Mass', 'name': 'Exact'}, self.record['props'])
 
     @property
     def monoisotopic_mass(self):
-        return parse_prop({'label':'Weight','name':'MonoIsotopic'}, self.record['props'])
+        return parse_prop({'label': 'Weight', 'name': 'MonoIsotopic'}, self.record['props'])
 
     @property
     def tpsa(self):
-        return parse_prop({'implementation':'E_TPSA'}, self.record['props'])
+        return parse_prop({'implementation': 'E_TPSA'}, self.record['props'])
 
     @property
     def complexity(self):
-        return parse_prop({'implementation':'E_COMPLEXITY'}, self.record['props'])
+        return parse_prop({'implementation': 'E_COMPLEXITY'}, self.record['props'])
 
     @property
     def h_bond_donor_count(self):
-        return parse_prop({'implementation':'E_NHDONORS'}, self.record['props'])
+        return parse_prop({'implementation': 'E_NHDONORS'}, self.record['props'])
 
     @property
     def h_bond_acceptor_count(self):
-        return parse_prop({'implementation':'E_NHACCEPTORS'}, self.record['props'])
+        return parse_prop({'implementation': 'E_NHACCEPTORS'}, self.record['props'])
 
     @property
     def rotatable_bond_count(self):
-        return parse_prop({'implementation':'E_NROTBONDS'}, self.record['props'])
+        return parse_prop({'implementation': 'E_NROTBONDS'}, self.record['props'])
 
     @property
     def fingerprint(self):
-        return parse_prop({'implementation':'E_SCREEN'}, self.record['props'])
+        return parse_prop({'implementation': 'E_SCREEN'}, self.record['props'])
 
     @property
     def heavy_atom_count(self):
@@ -354,66 +366,66 @@ class Compound(object):
     def volume_3d(self):
         conf = self.record['coords'][0]['conformers'][0]
         if 'data' in conf:
-            return parse_prop({'label':'Shape','name':'Volume'}, conf['data'])
+            return parse_prop({'label': 'Shape', 'name': 'Volume'}, conf['data'])
 
     @property
     def multipoles_3d(self):
         conf = self.record['coords'][0]['conformers'][0]
         if 'data' in conf:
-            return parse_prop({'label':'Shape','name':'Multipoles'}, conf['data'])
+            return parse_prop({'label': 'Shape', 'name': 'Multipoles'}, conf['data'])
 
     @property
     def conformer_rmsd_3d(self):
         coords = self.record['coords'][0]
         if 'data' in coords:
-            return parse_prop({'label':'Conformer','name':'RMSD'}, coords['data'])
+            return parse_prop({'label': 'Conformer', 'name': 'RMSD'}, coords['data'])
 
     @property
     def effective_rotor_count_3d(self):
-        return parse_prop({'label':'Count','name':'Effective Rotor'}, self.record['props'])
+        return parse_prop({'label': 'Count', 'name': 'Effective Rotor'}, self.record['props'])
 
     @property
     def pharmacophore_features_3d(self):
-        return parse_prop({'label':'Features','name':'Pharmacophore'}, self.record['props'])
+        return parse_prop({'label': 'Features', 'name': 'Pharmacophore'}, self.record['props'])
 
     @property
     def mmff94_partial_charges_3d(self):
-        return parse_prop({'label':'Charge','name':'MMFF94 Partial'}, self.record['props'])
+        return parse_prop({'label': 'Charge', 'name': 'MMFF94 Partial'}, self.record['props'])
 
     @property
     def mmff94_energy_3d(self):
         conf = self.record['coords'][0]['conformers'][0]
         if 'data' in conf:
-            return parse_prop({'label':'Energy','name':'MMFF94 NoEstat'}, conf['data'])
+            return parse_prop({'label': 'Energy', 'name': 'MMFF94 NoEstat'}, conf['data'])
 
     @property
     def conformer_id_3d(self):
         conf = self.record['coords'][0]['conformers'][0]
         if 'data' in conf:
-            return parse_prop({'label':'Conformer','name':'ID'}, conf['data'])
+            return parse_prop({'label': 'Conformer', 'name': 'ID'}, conf['data'])
 
     @property
     def shape_selfoverlap_3d(self):
         conf = self.record['coords'][0]['conformers'][0]
         if 'data' in conf:
-            return parse_prop({'label':'Shape','name':'Self Overlap'}, conf['data'])
+            return parse_prop({'label': 'Shape', 'name': 'Self Overlap'}, conf['data'])
 
     @property
     def feature_selfoverlap_3d(self):
         conf = self.record['coords'][0]['conformers'][0]
         if 'data' in conf:
-            return parse_prop({'label':'Feature','name':'Self Overlap'}, conf['data'])
+            return parse_prop({'label': 'Feature', 'name': 'Self Overlap'}, conf['data'])
 
     @property
     def shape_fingerprint_3d(self):
         conf = self.record['coords'][0]['conformers'][0]
         if 'data' in conf:
-            return parse_prop({'label':'Fingerprint','name':'Shape'}, conf['data'])
+            return parse_prop({'label': 'Fingerprint', 'name': 'Shape'}, conf['data'])
 
 
-def parse_prop(filter, proplist):
-    """ Extract property value from record using the given urn filter.  """
-    props = [i for i in proplist if all(item in i['urn'].items() for item in filter.items())]
+def parse_prop(search, proplist):
+    """ Extract property value from record using the given urn search filter.  """
+    props = [i for i in proplist if all(item in i['urn'].items() for item in search.items())]
     if len(props) > 0:
         return props[0]['value'][props[0]['value'].keys()[0]]
 
@@ -470,6 +482,7 @@ class Substance(object):
         results = json.loads(get(self.sid, 'sid', 'substance', 'aids'))
         return results['InformationList']['Information'][0]['AID']
 
+
 class Assay(object):
     def __init__(self, record):
         self.record = record
@@ -488,6 +501,7 @@ class Assay(object):
 
 class PubChemHTTPError(Exception):
     """ Generic error class to handle all HTTP error codes """
+
     def __init__(self, e):
         self.code = e.code
         self.msg = e.reason
@@ -514,33 +528,45 @@ class PubChemHTTPError(Exception):
 
 class BadRequestError(PubChemHTTPError):
     """ Request is improperly formed (syntax error in the URL, POST body, etc.) """
+
     def __init__(self, msg='Request is improperly formed'):
         self.msg = msg
 
+
 class NotFoundError(PubChemHTTPError):
     """ The input record was not found (e.g. invalid CID) """
+
     def __init__(self, msg='The input record was not found'):
         self.msg = msg
 
+
 class MethodNotAllowedError(PubChemHTTPError):
     """ Request not allowed (such as invalid MIME type in the HTTP Accept header) """
+
     def __init__(self, msg='Request not allowed'):
         self.msg = msg
 
+
 class TimeoutError(PubChemHTTPError):
     """ The request timed out, from server overload or too broad a request """
+
     def __init__(self, msg='The request timed out'):
         self.msg = msg
 
+
 class UnimplementedError(PubChemHTTPError):
     """ The requested operation has not (yet) been implemented by the server """
+
     def __init__(self, msg='The requested operation has not been implemented'):
         self.msg = msg
 
+
 class ServerError(PubChemHTTPError):
     """ Some problem on the server side (such as a database server down, etc.) """
+
     def __init__(self, msg='Some problem on the server side'):
         self.msg = msg
+
 
 if __name__ == '__main__':
     print __version__
--- pubchempy_test.py
+++ pubchempy_test.py
@@ -6,20 +6,16 @@ Python interface for the PubChem PUG REST service.
 https://github.com/mcs07/PubChemPy
 """
 
-
 import unittest
 
 from pubchempy import *
+
 
 class TestPubChemPy(unittest.TestCase):
     """ These tests don't check whether the output is correct. They just check that no exception is raised. """
 
     def setUp(self):
         """ Define some example inputs """
-        self.alaninesmiles = 'N[C@@H](C)C(=O)O'
-        self.morphineinchi = [{'value': 'InChI=1/C17H19NO3/c1-18-7-6-17-10-3-5-13(20)16(17)21-15-12(19)4-2-9(14(15)17)8-11(10)18/h2-5,10-11,13,16,19-20H,6-8H2,1H3/t10-,11+,13?,16-,17-/m0/s1', 'notation': 'Morphine', 'resolver': 'cir_name'}]
-        self.tntsmiles1 = '[N+](=O)([O-])C1=C(C(=CC(=C1)[N+](=O)[O-])[N+](=O)[O-])C'
-        self.tntsmiles2 = 'C1=C(C=C(C(=C1[N+]([O-])=O)C)[N+]([O-])=O)[N+]([O-])=O'
         self.phenanthrolinesmiles = 'C1=CC2=C(C3=C(C=CC=N3)C=C2)N=C1'
         self.molform = 'C10H21N'
         self.rucomplex = 'tris-(1,10-phenanthroline)ruthenium'
