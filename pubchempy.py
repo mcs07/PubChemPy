44ee8b17a5813b0684e8ed5b94d6fd7b09e97700
Matt Swain <m.swain@me.com>
2025-07-31 14:28:01 -0400

    Better HTTP error handling
    Add support for HTTP code 503 ServerBusy and refactor the functionality that raises the correct subclass from the HTTPError.

--- pubchempy.py
+++ pubchempy.py
@@ -281,7 +281,7 @@ def request(identifier, namespace='cid', domain='compound', operation=None, outp
         response = urlopen(apiurl, postdata, context=context)
         return response
     except HTTPError as e:
-        raise PubChemHTTPError(e)
+        raise create_http_error(e) from e
 
 
 def get(identifier, namespace='cid', domain='compound', operation=None, output='JSON', searchtype=None, **kwargs):
@@ -1332,49 +1332,73 @@ class ResponseParseError(PubChemPyError):
     """PubChem response is uninterpretable."""
     pass
 
-
 class PubChemHTTPError(PubChemPyError):
-    """Generic error class to handle all HTTP error codes."""
-    def __init__(self, e):
-        self.code = e.code
-        self.msg = e.reason
-        try:
-            self.msg += ': %s' % json.loads(e.read().decode())['Fault']['Details'][0]
-        except (ValueError, IndexError, KeyError):
-            pass
-        if self.code == 400:
-            raise BadRequestError(self.msg)
-        elif self.code == 404:
-            raise NotFoundError(self.msg)
-        elif self.code == 405:
-            raise MethodNotAllowedError(self.msg)
-        elif self.code == 504:
-            raise TimeoutError(self.msg)
-        elif self.code == 501:
-            raise UnimplementedError(self.msg)
-        elif self.code == 500:
-            raise ServerError(self.msg)
+    """Generic error class to handle HTTP error codes."""
+    def __init__(self, code: int, msg: str, details: list[str]) -> None:
+        super().__init__(msg)
+        self.code = code
+        self.msg = msg
+        self.details = details
 
-    def __str__(self):
-        return repr(self.msg)
+    def __str__(self) -> str:
+        output = f'PubChem HTTP Error {self.code} {self.msg}'
+        if self.details:
+            details = ', '.join(self.details)
+            output = f'{output} ({details})'
+        return output
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({self.code!r}, {self.msg!r}, {self.details!r})'
+
+
+def create_http_error(e: HTTPError) -> PubChemHTTPError:
+    """Create appropriate PubChem HTTP error subclass based on status code."""
+    code = e.code
+    msg = e.msg
+    details = []
+    try:
+        fault = json.loads(e.read().decode())['Fault']
+        msg = fault.get('Code', msg)
+        if 'Message' in fault:
+            msg = f'{msg}: {fault["Message"]}'
+        details = fault.get('Details', [])
+    except (ValueError, IndexError, KeyError):
+        pass
+
+    error_map = {
+        400: BadRequestError,
+        404: NotFoundError,
+        405: MethodNotAllowedError,
+        500: ServerError,
+        501: UnimplementedError,
+        503: ServerBusyError,
+        504: TimeoutError,
+    }
+    error_class = error_map.get(code, PubChemHTTPError)
+    return error_class(code, msg, details)
 
 
 class BadRequestError(PubChemHTTPError):
     """Request is improperly formed (syntax error in the URL, POST body, etc.)."""
-    def __init__(self, msg='Request is improperly formed'):
-        self.msg = msg
 
 
 class NotFoundError(PubChemHTTPError):
     """The input record was not found (e.g. invalid CID)."""
-    def __init__(self, msg='The input record was not found'):
-        self.msg = msg
 
 
 class MethodNotAllowedError(PubChemHTTPError):
     """Request not allowed (such as invalid MIME type in the HTTP Accept header)."""
-    def __init__(self, msg='Request not allowed'):
-        self.msg = msg
+
+class ServerError(PubChemHTTPError):
+    """Some problem on the server side (such as a database server down, etc.)."""
+
+
+class UnimplementedError(PubChemHTTPError):
+    """The requested operation has not (yet) been implemented by the server."""
+
+
+class ServerBusyError(PubChemHTTPError):
+    """Too many requests or server is busy, retry later."""
 
 
 class TimeoutError(PubChemHTTPError):
@@ -1382,20 +1406,6 @@ class TimeoutError(PubChemHTTPError):
 
     See :ref:`Avoiding TimeoutError <avoiding_timeouterror>` for more information.
     """
-    def __init__(self, msg='The request timed out'):
-        self.msg = msg
-
-
-class UnimplementedError(PubChemHTTPError):
-    """The requested operation has not (yet) been implemented by the server."""
-    def __init__(self, msg='The requested operation has not been implemented'):
-        self.msg = msg
-
-
-class ServerError(PubChemHTTPError):
-    """Some problem on the server side (such as a database server down, etc.)."""
-    def __init__(self, msg='Some problem on the server side'):
-        self.msg = msg
 
 
 if __name__ == '__main__':
