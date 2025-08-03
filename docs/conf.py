b12163b96af51ed478804b53ab4833c7efac538b
Matt Swain <m.swain@me.com>
2025-08-03 16:05:10 -0400

    Update docs config
    Support google-style docstrings, add github repo links, module docstring, ruff format

--- docs/conf.py
+++ docs/conf.py
@@ -1,52 +1,58 @@
-# Configuration file for the Sphinx documentation builder.
+"""Configuration file for the Sphinx documentation builder."""
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import sys
 import os
+import sys
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here.
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath("../src"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'PubChemPy'
-copyright = '2014-2025, Matt Swain'
-author = 'Matt Swain'
-release = '1.0.4'
+project = "PubChemPy"
+copyright = "2014-2025, Matt Swain"
+author = "Matt Swain"
+release = "1.0.4"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.ifconfig',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.ifconfig",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ['_static']
+html_static_path = ["_static"]
+html_theme_options = {
+    "source_repository": "https://github.com/mcs07/PubChemPy",
+    "source_branch": "main",
+    "source_directory": "docs/",
+}
 
 # -- Options for autodoc ----------------------------------------------------
 
 # Sort autodoc members by the order they appear in the source code
-autodoc_member_order = 'bysource'
+autodoc_member_order = "bysource"
 
 # Concatenate the class and __init__ docstrings together
-autoclass_content = 'both'
+autoclass_content = "both"
 
 # -- Options for intersphinx -------------------------------------------------
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
 }
