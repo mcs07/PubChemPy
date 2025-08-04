b728ffa84ae01fdcec59e90ff86b4531911ff074
Matt Swain <m.swain@me.com>
2025-08-04 17:03:40 -0400

    pytest rerun URLError (#132)
    * Add URLError to the list of exceptions to rerun tests for
    * Re-enable parallel tests in GitHub Actions CI
    * Disable rate-limiter that pauses 1 second between tests
    
    It looks like the rerun functionality is enough to ensure the tests pass, even without rate-limiting and running in serial.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,17 @@
 """Pytest configuration for PubChemPy tests."""
 
-import time
+# import time
 
-import pytest
+# import pytest
 
 
-@pytest.fixture(autouse=True)
-def rate_limit_tests():
-    """Limit the rate of requests to PubChem.
-
-    This fixture ensures that there is a delay between tests to avoid hitting
-    PubChem's rate limits, which can cause PubChemHTTPError: 'PUGREST.ServerBusy' to be
-    raised. A delay of 1 second is automatically applied between every test.
-    """
-    yield
-    time.sleep(1)
+# @pytest.fixture(autouse=True)
+# def rate_limit_tests():
+#     """Limit the rate of requests to PubChem.
+#
+#     This fixture ensures that there is a delay between tests to avoid hitting
+#     PubChem's rate limits, which can cause PubChemHTTPError: 'PUGREST.ServerBusy' to be
+#     raised. A delay of 1 second is automatically applied between every test.
+#     """
+#     yield
+#     time.sleep(1)
